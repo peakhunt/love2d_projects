c4948fe45ca35715d5378aaeae473f1b062b16d0
Huckleberry K <hawk.kim1974@gmail.com>
2022-04-27 11:32:20 +0900

    added score graphics

---
+++ kungfumaster/entities/score.lua
@@ -0,0 +1,32 @@
+local asset_conf = require('asset_conf')
+local utils = require('utils')
+local state = require('state')
+local resource = require('resource')
+local viewport = require('viewport')
+
+return function(vx, vy, score)
+  local entity = {
+    score = string.format("%d", score),
+    vx = vx,
+    vy = vy,
+    timeAccumulated = 0,
+    dead = false,
+
+    update = function(self, dt)
+      self.timeAccumulated = self.timeAccumulated + dt
+
+      if self.timeAccumulated > 1 then
+        self.dead = true
+      end
+    end,
+
+    draw = function(self)
+      local px, py = viewport:virtualPointToScreenCoord(self.vx, self.vy)
+
+      love.graphics.setColor(love.math.colorFromBytes(0xad, 0xd8, 0xe6))
+      love.graphics.print(self.score, resource.font, px, py)
+    end,
+  }
+
+  return entity
+end
